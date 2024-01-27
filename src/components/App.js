00735eb397fdd6dc5de9ec7eb682981193ee8967
Your NameRam181999 <ramprasadgoudaineri@gmail.com>
2024-01-27 08:31:22 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,63 @@
 
-import React from "react";
+import React,{useEffect,useState} from "react";
 import './../styles/App.css';
+import "regenerator-runtime/runtime";
+
 
 const App = () => {
+
+  const [people,setPeople]=useState([]);
+  const getUsers=async()=>{
+const res = await fetch("https://reqres.in/api/users");
+const response =await res.json()
+console.log(response.data)
+setPeople(response.data)
+  }
+
+//   useEffect(()=>{
+
+// getUsers()
+
+//   },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+      <header className="nav">
+        <h1>Blue Whales</h1>
+        <button className="btn" onClick={getUsers}>Get User List</button>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.length===0? ("No data found to display"): 
+           
+              people.map((person,idx)=>{
+                return (
+                  <tr key={idx}>
+                    <td>{person.first_name}</td>
+                    <td>{person.last_name}</td>
+                    <td>{person.email}</td>
+                    <td><img src={person.avatar}></img></td>
+                  </tr>
+                );
+              })
+            
+          
+         
+          
+          
+          
+          }
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
 export default App
